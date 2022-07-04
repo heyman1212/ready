265bf1ad9cd5ba70a44e714aa42c35cd1fc78b78
heyman1212 <107253355+heyman1212@users.noreply.github.com>
2022-07-05 04:00:18 +0600

    Add files via upload

---
+++ AKING.py
@@ -0,0 +1,14 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+    os.system('git pull')
+    import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print("\n\x1b[1;92m Assalamu alaikum Congratulations ! Your Device Support this Tools")
+    print(' Join our Facebook Group For Any Help 🙋 thanks ')
+    os.system('xdg-open https://www.facebook.com/groups/351076900316263/?ref=share');time.sleep(3)
+    from FIRE import Subscraption
+    Subscraption()
